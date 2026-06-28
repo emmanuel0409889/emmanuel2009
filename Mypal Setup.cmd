348070769d73e9e10480bd18ad0154402eb72cd1
emmanuel0409889 <292593339+emmanuel0409889@users.noreply.github.com>
2026-06-28 10:38:45 -0700

    Reduce timeout duration in setup script

--- Mypal Setup.cmd
+++ Mypal Setup.cmd
@@ -4,5 +4,5 @@ pause
 echo Do want io install it?
 pause
 Please Wait
-timeout /t 8
+timeout /t 5 /nobreak > nul
 start  https://github.com/Feodor2/Mypal68/releases/download/68.14.3b/mypal-68.14.3.en-US.win32.zip
