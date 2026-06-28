31f8a666891e14d191b5cefd1d25d55f4195a69a
emmanuel0409889 <292593339+emmanuel0409889@users.noreply.github.com>
2026-06-28 12:32:38 -0700

    Fix echo and timeout messages in setup script

--- Mypal Setup.cmd
+++ Mypal Setup.cmd
@@ -3,6 +3,6 @@ echo This is the Mypal setup do you want to continue?
 pause
 echo Do want io install it?
 pause
-Please Wait
-timeout /t 5 /nobreak > nul
+echo Please Wait
+timeout /t 9 /nobreak > nul
 start  https://github.com/Feodor2/Mypal68/releases/download/68.14.3b/mypal-68.14.3.en-US.win32.zip
